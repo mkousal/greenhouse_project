cbc22158532158f7678faa7e76f4dd68f6f37a93
mkousal <mkousal1@gmail.com>
2021-11-24 11:20:30 +0100

    Init atmel studio project

---
+++ fw/greenhouse/greenhouse/main.c
@@ -0,0 +1,18 @@
+/*
+ * greenhouse.c
+ *
+ * Created: 24.11.2021 11:19:26
+ * Author : Martin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
