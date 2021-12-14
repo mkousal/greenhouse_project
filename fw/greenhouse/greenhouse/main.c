58ef8403573335cc50f132095bf3906bc23d61f9
Martin Kousal <mkousal1@gmail.com>
2021-12-14 22:19:58 +0100

    Add comments to code

--- fw/greenhouse/greenhouse/main.c
+++ fw/greenhouse/greenhouse/main.c
@@ -20,7 +20,7 @@
 #include "relay.h"				// Relay library with basic functions with relay shield
 #include "dht.h"				// DHT12 specific library with basic functions over I2C
 #include "lcd.h"				// Peter Fleury's LCD library
-#include "adc.h"
+#include "adc.h"				// ADC library with basic functions and settings
 
 volatile uint8_t minuteFlag = 0;
 volatile int temperature = 0;
@@ -28,12 +28,14 @@ volatile uint8_t humidity = 0;
 volatile uint16_t soilMoisture = 0;
 volatile uint16_t lightIntensity = 0;
 
+// Function map for getting results from one range to the another
 uint16_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max)
 {
 	return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void lcdUpdate(uint16_t temperature, uint16_t soil, uint16_t light, uint16_t hum)
+// Function to update LCD with new values
+void lcdUpdate(int temperature, uint16_t soil, uint16_t light, uint16_t hum)
 {
 	char tmp_str[4] = "    ";
 	lcd_clrscr();
@@ -45,14 +47,45 @@ void lcdUpdate(uint16_t temperature, uint16_t soil, uint16_t light, uint16_t hum
 	lcd_puts_P("H: ");
 	itoa(hum, tmp_str, 10);
 	lcd_puts(tmp_str);
+	lcd_gotoxy(8, 0);
+	lcd_puts_P("S: ");
+	itoa(soil, tmp_str, 10);
+	lcd_puts(tmp_str);
+	lcd_gotoxy(8, 1);
+	lcd_puts_P("L: ");
+	itoa(light, tmp_str, 10);
+	lcd_puts(tmp_str);
+}
+
+// Function to print all measured values to UART
+void printUART(int temperature, uint16_t soil, uint16_t light, uint16_t hum)
+{
+	char tmp_str[5] = "     ";
+	itoa(temperature, tmp_str, 10);
+	uart_puts(tmp_str);
+	uart_putc(248);		// Degree sign
+	uart_puts_P("C  Air humidity: ");
+	itoa(humidity, tmp_str, 10);
+	uart_puts(tmp_str);
+	uart_puts_P("%  Soil humidity: ");
+	itoa(soilMoisture, tmp_str, 10);
+	uart_puts(tmp_str);
+	uart_puts_P("%  Light: ");
+	if (lightIntensity > 600)
+	uart_puts_P("dark");
+	else
+	uart_puts_P("light");
+	itoa(lightIntensity, tmp_str, 10);
+	uart_puts(tmp_str);
+	uart_puts_P("\r\n");
 }
 
 int main(void)
 {
-	relay_init();
-	twi_init();
-	uart_init(UART_BAUD_SELECT(9600, F_CPU));
-	lcd_init(LCD_DISP_ON);
+	relay_init();		// Set ports for relay controlling
+	twi_init();			// Initialize TWI
+	uart_init(UART_BAUD_SELECT(9600, F_CPU));	// Initialize UART with 9600 buad
+	lcd_init(LCD_DISP_ON);		// Initialize LCD
 	
 	uart_puts_P("\r\nGreenhouse system started\r\n");	// Print start message to UART
 	lcd_gotoxy(5,0);									// Print starting message to LCD
@@ -62,88 +95,66 @@ int main(void)
 	_delay_ms(1000);
 	lcd_clrscr();
 	
-	ADC_set_reference_AVCC();
-	ADC_enable();
-	ADC_enable_conversion_complete_interrupt();
-	ADC_set_prescaler_128();
+	ADC_set_reference_AVCC();						// Set ADC reference
+	ADC_enable();									// Enable ADC
+	ADC_enable_conversion_complete_interrupt();		// Enable interrupt after AD conversion complete
+	ADC_set_prescaler_128();						// Set ADC prescaler to 128
 	
-	TIM1_overflow_4s();
-	TIM1_overflow_interrupt_enable();
-	TIM0_overflow_16ms();
-	TIM0_overflow_interrupt_enable();
-	sei();
+	TIM1_overflow_4s();								// Set Timer 1 overflow to 4 s
+	TIM1_overflow_interrupt_enable();				// Enable Timer 1 overflow interrupt
+	TIM0_overflow_16ms();							// Set Timer 0 overflow to 16 ms
+	TIM0_overflow_interrupt_enable();				// Enable Timer 0 overflow interrupt
+	sei();											// Globally enable interrupt flag
 
     while (1) 
     {
 		if (minuteFlag == 1)	// 1 minute interval
 		{
-			minuteFlag = 0;
-			temperature = DHT_getTemperature();
-			humidity = DHT_getHumidity();
-			soilMoisture = map(1023 - soilMoisture, 1023 - 940, 1023 - 730, 0, 100);
+			minuteFlag = 0;		// Reset 1 minute flag
+			temperature = DHT_getTemperature();	// Get temperature from DHT12
+			humidity = DHT_getHumidity();		// Get humidity from DHT12
+			soilMoisture = map(1023 - soilMoisture, 1023 - 940, 1023 - 730, 0, 100);	// Get measured soil moisture values into 0-100%
 			uart_puts_P("Temperature: ");
 		
-			char tmp_str[5] = "     ";
-			itoa(temperature, tmp_str, 10);
-			uart_puts(tmp_str);
-			uart_puts_P("°C  Air humidity: ");
-			itoa(humidity, tmp_str, 10);
-			uart_puts(tmp_str);
-			uart_puts_P("%  Soil humidity: ");
-			itoa(soilMoisture, tmp_str, 10);
-			uart_puts(tmp_str);
-			uart_puts_P("%  Light: ");
-			if (lightIntensity > 600)
-				uart_puts_P("dark");
-			else
-				uart_puts_P("light");
-			itoa(lightIntensity, tmp_str, 10);
-			uart_puts(tmp_str);
-			uart_puts_P("\r\n");
-			
 			if (lightIntensity > 600)	// Light intensity -> dark
 				relay_on(RELAY_3);		// Turn on light (relay 3)
 			else						// Light intensity -> bright
-				relay_off(RELAY_3);		// turn off light
+				relay_off(RELAY_3);		// turn off light		
 				
-			
-			itoa(soilMoisture, tmp_str, 10);
-			uart_puts(tmp_str);
-			uart_puts_P("\r\n");
-			
-			if (soilMoisture < 50)
-				relay_on(RELAY_4);
-			else
-				relay_off(RELAY_4);
+			if (soilMoisture < 50)		// Soil moisture below 50%
+				relay_on(RELAY_4);		// Turn on sprinkler (relay 4)
+			else						// Soil moisture > 50%	
+				relay_off(RELAY_4);		// Turn off sprinkler
 				
-			if (temperature > 25 || humidity > 30)
-				relay_on(RELAY_1);
-			else
-				relay_off(RELAY_1);
+			if (temperature > 25 || humidity > 30)	// If temperature > 25°C or humidity of air > 30%
+				relay_on(RELAY_1);					// Open window (relay 1)
+			else									// Temperature < 25°C or humidity of air < 30%	
+				relay_off(RELAY_1);					// Close window (relay 1)
 				
-			if(temperature < 25)
-				relay_on(RELAY_2);
-			else
-				relay_off(RELAY_2);
+			if(temperature < 25)					// If temperature > 25°C
+				relay_on(RELAY_2);					// Turn on heater (relay 2)
+			else									// Temperature < 25°C
+				relay_off(RELAY_2);					// Turn off heater
 				
 				
-			lcdUpdate(temperature, soilMoisture, lightIntensity, humidity);
+			lcdUpdate(temperature, soilMoisture, lightIntensity, humidity);	// Update LCD with new measured values
+			printUART(temperature, soilMoisture, lightIntensity, humidity);	// Update UART with new measured values
 		}
     }
 }
 
-ISR(TIMER1_OVF_vect)
+ISR(TIMER1_OVF_vect)	// ISR for setting every one minute flag
 {
 	static uint8_t cnt = 0;
 	cnt++;
-	if (cnt == 2)	// 16 for 1 minute interval
+	if (cnt == 2)	// '16' for 1 minute interval, '2' for testing => 8s
 	{
 		cnt = 1;
 		minuteFlag = 1;
 	}
 }
 
-ISR(TIMER0_OVF_vect)
+ISR(TIMER0_OVF_vect) // Start AD convert of soil moisture or light intensity, every thing is measured once in 320 ms
 {
 	static uint8_t cnt = 0;
 	static uint8_t i = 0;
@@ -155,12 +166,12 @@ ISR(TIMER0_OVF_vect)
 		{
 			case 0:
 				ADC_select_channel_3();	// Soil moisture sensor
-				ADC_start();
+				ADC_start();			// Start AD convert
 				i = 1;
 				break;
 			case 1:
 				ADC_select_channel_2();	// Light intensity sensor
-				ADC_start();
+				ADC_start();			// Start AD convert
 				i = 0;
 				break;
 		}
